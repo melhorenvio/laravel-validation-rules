e14a2db4177b28feb372d127b5ac64c50cd78b9c
Philippe Bachilli <philippe.bachilli@melhorenvio.com>
2023-11-03 10:50:52 -0300

    fix: cnpj validation and phpunit tests

--- src/Rules/Cnpj.php
+++ src/Rules/Cnpj.php
@@ -33,14 +33,14 @@ class Cnpj implements Rule
     public function passes($attribute, $value): bool
     {
         if (empty($value)) {
-            return true;
+            return false;
         }
 
         if ($this->allowMask) {
             $value = preg_replace('/\D/', '', $value);
         }
 
-        if (!($value && mb_strlen($value) === 14)) {
+        if (mb_strlen($value) !== 14) {
             return false;
         }
 
